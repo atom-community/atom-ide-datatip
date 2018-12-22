29a936f2dbf6adb2a03fbe3896d4d1e7a43a18b7
Andreas Gerlach <andi@appelgriebsch.org>
2018-12-22 19:59:19 +0100

    Initial port of Nuclide/Atom-IDE package for displaying data tips

---
+++ spec/atom-ide-datatips-spec.js
@@ -0,0 +1,18 @@
+'use babel';
+
+import AtomIdeDatatips from '../lib/atom-ide-datatips';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('AtomIdeDatatips', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('atom-ide-datatips');
+  });
+
+});
