3a8a36be51662a7c649e8e9d80475ecc6dc602ec
aminya <aminyahyaabadi74@gmail.com>
2020-04-21 18:44:45 -0500

    feat: babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+let presets = ["@babel/preset-react"];
+
+let plugins =  [
+  // "transform-react-remove-prop-types",
+  // "@babel/plugin-syntax-dynamic-import",
+  // '@babel/plugin-proposal-object-rest-spread',
+  // '@babel/plugin-proposal-optional-chaining',
+  // '@babel/plugin-proposal-class-properties'
+  ];
+
+if (process.env.BABEL_ENV === "development") {
+  plugins.push("@babel/plugin-transform-modules-commonjs")
+}
+
+module.exports = {
+  presets: presets,
+  plugins: plugins,
+  exclude: "node_modules/**",
+  // runtimeHelpers: true,
+}
