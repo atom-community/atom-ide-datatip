22e6cc6c75a6ad6389afb814a03d63d9197948b3
aminya <aminyahyaabadi74@gmail.com>
2020-04-27 02:34:34 -0500

    chore: separating benchmark from spec

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,26 +1,21 @@
-describe("atom-ide-datatip", () => {
+describe("atom-ide-datatip tests", () => {
 
-    beforeEach(async () => {
-        jasmine.attachToDOM(atom.views.getView(atom.workspace))
-        // Activate activation hook
-        atom.packages.triggerActivationHook('core:loaded-shell-environment');
-        atom.packages.triggerDeferredActivationHooks();
-        // Activate package-deps
-        await atom.packages.activatePackage("busy-signal")
-        await atom.packages.activatePackage("atom-ide-markdown-service")
-    })
+  beforeEach(async () => {
+    jasmine.attachToDOM(atom.views.getView(atom.workspace))
 
-    it("Activation", async function () {
+    /*    Activation     */
+    // Trigger deffered activation
+    atom.packages.triggerDeferredActivationHooks();
+    // Activate activation hook
+    atom.packages.triggerActivationHook('core:loaded-shell-environment');
+    // Activate package-deps
+    await atom.packages.activatePackage("busy-signal")
+    await atom.packages.activatePackage("atom-ide-markdown-service")
+    // Activate atom-ide-datatip
+    await atom.packages.activatePackage("atom-ide-datatip");
+  })
 
-        // This makes the log visible again from the command line.
-        spyOn(console, 'log').and.callThrough();
-
-        console.log("\n Activation Started")
-        measure("Activation Time", async function activationBenchmark() {
-            await atom.packages.activatePackage("atom-ide-datatip")
-        })
-        expect(atom.packages.isPackageLoaded("atom-ide-datatip")).toBeTruthy()
-
-        console.log("\n Finished")
-    })
+  it("Activation", async function() {
+    expect(atom.packages.isPackageLoaded("atom-ide-datatip")).toBeTruthy();
+  })
 })
