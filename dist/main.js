67c24a3d4bf73cbe706090febdb732ecfab319cd
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-07-03 03:55:08 -0500

    chore: add built files

---
+++ dist/main.js
@@ -0,0 +1,2 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),require("atom");var e=require("./main-3aa97179.js");require("stream"),exports.activate=e.activate,exports.config=e.config,exports.consumeMarkdownRenderer=e.consumeMarkdownRenderer,exports.deactivate=e.deactivate,exports.provideDatatipService=e.provideDatatipService;
+//# sourceMappingURL=main.js.map
