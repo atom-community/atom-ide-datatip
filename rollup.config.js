7972b650f1c2c4a186884d48fba2966a66ff1c6d
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-07-03 05:14:04 -0500

    chore: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,32 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-// import peerexternal from 'rollup-plugin-peer-deps-external';
 
 import babel from "rollup-plugin-babel";
 // import typescript from '@rollup/plugin-typescript';
-// import coffeescript from 'rollup-plugin-coffee-script';
 
 import { terser } from "rollup-plugin-terser";
 
-// import pkg from './package.json';
-
 let plugins = [
-  babel(),
-
-  // // datatip loads faster without this
-  // peerexternal({
-  //   includeDependencies: true,
+  // // so Rollup can convert TypeScript to JavaScript
+  // typescript({
+  //   noEmitOnError: false,
   // }),
 
-  // so Rollup can find externals
-  resolve({ extensions: [".js", ".coffee"], preferBuiltins: true }),
-
-  // so Rollup can convert externals to an ES module
-  commonjs({
-    // undetected named exports
-    namedExports: {
-      // left-hand side can be an absolute path, a path relative to the current directory, or the name of a module in node_modules
-      "react-dom/server": ["renderToStaticMarkup"],
-      // "dompurify": ["sanitize"] // doesn't work
-    },
-  }),
-
-  // // Convert CoffeeScript to JavaScript
+  // if any (in deps as well): Convert CoffeeScript to JavaScript
   // coffeescript(),
 
-  // // Convert TypeScript to JavaScript
-  // typescript(
-  //   { noEmitOnError: false }
+  // // so Rollup can bundle JSON to JavaScript
+  // json(
+  //   { compact: true }
   // ),
+
+  babel(),
+
+  // so Rollup can find externals
+  resolve({ extensions: ["ts", ".js", ".coffee"], preferBuiltins: true }),
+
+  // so Rollup can convert externals to an ES module
+  commonjs(),
 ];
 
 // minify only in production mode
