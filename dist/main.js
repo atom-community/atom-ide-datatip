ab48f946bd61ebd0424965c9c1666728f234dd6a
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-10-04 19:27:13 -0500

    chore: delete old built files

--- dist/main.js
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),require("atom");var e=require("./main-7eb87ac7.js");require("stream"),exports.activate=e.activate,exports.config=e.config,exports.consumeMarkdownRenderer=e.consumeMarkdownRenderer,exports.deactivate=e.deactivate,exports.provideDatatipService=e.provideDatatipService;
-//# sourceMappingURL=main.js.map
