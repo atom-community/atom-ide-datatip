b9bd8aa5b4c742fcad1201c485ca5929a457e987
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-03-15 17:35:54 -0500

    fix: update atom dependencies

--- spec/runner.js
+++ spec/runner.js
@@ -1,4 +1,5 @@
 "use babel"
+// eslint-disable-next-line import/named
 import { createRunner } from "atom-jasmine3-test-runner"
 
 // https://github.com/UziTech/atom-jasmine3-test-runner#api
@@ -6,16 +7,4 @@ export default createRunner({
   testPackages: ["atom-ide-markdown-service", "busy-signal"],
   timeReporter: true,
   specHelper: true,
-  attachToDOM: true,
-  // Extra Packages
-  customMatchers: true,
-  jasmineFocused: false,
-  jasmineJson: false,
-  jasminePass: false,
-  jasmineShouldFail: false,
-  jasmineTagged: false,
-  mockClock: true,
-  mockLocalStorage: false,
-  profile: true,
-  unspy: false,
 })
