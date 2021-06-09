6030cc4213827bee80f258fdfa02383e053d319a
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-06-09 00:03:34 -0500

    fix: fix package-deps loading

--- lib/main.ts
+++ lib/main.ts
@@ -5,39 +5,43 @@ import type { DatatipService } from "atom-ide-base"
 export { default as config } from "./config.json"
 
 /** [subscriptions description] */
-let subscriptions: CompositeDisposable
+const subscriptions: CompositeDisposable = new CompositeDisposable()
 /** [datatipManager description] */
-let datatipManager: DataTipManager
+let datatipManager: DataTipManager | undefined
 
 /** Called by Atom when activating an extension */
 export function activate() {
   // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
-  subscriptions = new CompositeDisposable()
-  if (!datatipManager) {
+  if (datatipManager === undefined) {
     datatipManager = new DataTipManager()
   }
   subscriptions.add(datatipManager)
 
-  install_deps().then(() => {
-    datatipManager.initialize()
-  })
+  install_deps()
+    .then(() => {
+      datatipManager!.initialize()
+    })
+    .catch((e) => {
+      console.error(e)
+    })
 }
 
 async function install_deps() {
   // install package-deps if not loaded
   if (!atom.packages.isPackageLoaded("busy-signal")) {
     // Dynamic import https://mariusschulz.com/blog/dynamic-import-expressions-in-typescript
-    await import("atom-package-deps").then((atom_package_deps) => {
-      atom_package_deps.install("atom-ide-datatip", true)
-    })
+    const atom_package_deps = await import("atom-package-deps")
+    try {
+      await atom_package_deps.install("atom-ide-datatip", true)
+    } catch (err) {
+      atom.notifications.addError(err)
+    }
   }
 }
 
 /** Called by Atom when deactivating an extension */
 export function deactivate() {
-  if (subscriptions) {
-    subscriptions.dispose()
-  }
+  subscriptions.dispose()
 }
 
 /**
